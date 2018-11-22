a109783efdbc39d10d1a34b51fc1959af7acee4a
frclasso <frcalsso@yahoo.com.br>
2018-11-22 19:33:50 -0200

    bitwise operators

--- cap05-operadores-basicos/05_op_bitwise.py
+++ cap05-operadores-basicos/05_op_bitwise.py
@@ -3,18 +3,14 @@
 
 a = 60  # 60 = 0011 1100
 b = 13  # 13 = 0000 1101
-print('a=',a,':',bin(a),'b=',b,':',bin(b))
-c = 0
-c = a & b  # 12 = 0000 1100
-print("result of AND is ", c,':',bin(c))
-c = a | b  # 61 = 0011 1101
-print("result of OR is ", c,':',bin(c))
+print(format(a, '08b'))
+print(format(b, '08b'))
 
-c = a ^ b  # 49 = 0011 0001
-print("result of EXOR is ", c,':',bin(c))
-c = ~a  # -61 = 1100 0011
-print("result of COMPLEMENT is ", c,':',bin(c))
-c = a << 2  # 240 = 1111 0000
-print("result of LEFT SHIFT is ", c,':',bin(c))
-c = a >> 2  # 15 = 0000 1111
-print("result of RIGHT SHIFT is ", c,':',bin(c))
+
+print('a AND B => ',a & b,'binario => ', format(a & b, '08b'))
+print('a OR B => ', a | b,'binario => ', format(a | b, '08b'))
+print('a XOR B => ',a ^ b,'binario => ', format(a ^ b, '08b'))
+print('Complemento de a = >',~a,'binario = >', format(~a, '09b'))
+print('Deslocamento para esquerda a << 2', a << 2, 'binario', format(a << 2, '08b'))
+print('Deslocamento para direita a >> 2', a << 2, 'binario', format(a >> 2, '08b'))
+
