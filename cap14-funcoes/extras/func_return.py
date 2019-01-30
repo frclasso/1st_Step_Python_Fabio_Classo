0d3d440212b630b760e48f575cdc67d08056dcce
frclasso <frcalsso@yahoo.com.br>
2019-01-30 17:03:45 -0200

    add .csv files

---
+++ cap14-funcoes/extras/func_return.py
@@ -0,0 +1,9 @@
+def maximun(x,y):
+	if x > y:
+		return x
+	elif x == y:
+		return "The numbers are equal."
+	else:
+		return y
+
+print(maximun(2,3))
--- cap14-funcoes/extras/generator2.py
+++ cap14-funcoes/extras/generator2.py
@@ -1,30 +1,29 @@
-#!/usr/bin/python
-
-
 def main():
-    print("This is the function")
-    for i in inclusive_range():
+    print('Funcao inclusive range')
+    for i in inclusive_range(0,25,5,5):
         print(i, end=',')
 
 
 def inclusive_range(*args):
     numargs = len(args)
-    if numargs < 1: raise TypeError('Requires at least one argument')
+    if numargs < 1:
+        raise TypeError("Ao menos um argumento deve ser inserido")
     elif numargs == 1:
         start = 0
-        step =1
         stop = args[0]
+        step = 1
     elif numargs == 2:
         (start, stop) = args
         step = 1
     elif numargs == 3:
         (start, stop, step) = args
     else:
-        raise TypeError("inclusive _range expected at most 3 amrguments, got {}".format(numargs))
-    i =start
+        raise TypeError("inclusive_range espera 3 argumentos,"
+                        " recebeu:{}".format(numargs))
+
+    i = start
     while i <= stop:
         yield i
         i += step
 
-
-if __name__ == "__main__":main()
+if __name__=="__main__":main()
